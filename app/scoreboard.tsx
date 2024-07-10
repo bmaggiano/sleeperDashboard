5ec2058f89390c1c25bd84a032ef8a8cac135dde
bmaggiano <brandon.maggiano@gmail.com>
2024-07-10 11:04:26 -0700

    implemented interface on scoredboard as well as brought in matchup card component

--- app/scoreboard.tsx
+++ app/scoreboard.tsx
@@ -2,10 +2,18 @@ import { useEffect, useState } from "react";
 import { useAtom } from "jotai";
 import { valueAtom, leagueAtom, weekAtom } from "@/app/atoms/atom";
 import { getMatchups } from "./utils";
+import MatchupCard from "@/components/ui/matchupCard";
 
 interface Matchup {
     matchup_id: string;
     points: number;
+    user?: {
+        avatar: string;
+        display_name: string;
+        metadata: {
+            team_name: string;
+        },
+    }
 }
 
 export default function Scoreboard() {
@@ -19,15 +27,24 @@ export default function Scoreboard() {
             setScoresData(data);
         }
         fetchData();
-        console.log(scoresData)
     }, [weekIndex]);
 
     return (
         <div>
             <h1>Scoreboard</h1>
-            <h2>Player 1: {scoresData?.[0].points}</h2>
-            <h2>Player 2: {scoresData?.[1].points}</h2>
-            <h2>AUGHHHHH</h2>
+            {scoresData?.map((matchup) => {
+                return (
+                    <h2 key={matchup.matchup_id}>
+                        {matchup.user ? matchup.user.metadata.team_name : "Unknown Team"} {matchup.points}
+                    </h2>
+                )
+            })}
+            <MatchupCard team1={scoresData?.[0]} team2={scoresData?.[1]} />
+            <MatchupCard team1={scoresData?.[2]} team2={scoresData?.[3]} />
+            <MatchupCard team1={scoresData?.[4]} team2={scoresData?.[5]} />
+            <MatchupCard team1={scoresData?.[6]} team2={scoresData?.[7]} />
+            <MatchupCard team1={scoresData?.[8]} team2={scoresData?.[9]} />
+            <MatchupCard team1={scoresData?.[10]} team2={scoresData?.[11]} />
             {scoresData ? JSON.stringify(scoresData, null, 2) : "Loading..."}
         </div>
     );
