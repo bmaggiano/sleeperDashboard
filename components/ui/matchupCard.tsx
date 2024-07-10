1f266a2ed5614f8c9bc6cfaa2d633686b8a4d5d9
bmaggiano <brandon.maggiano@gmail.com>
2024-07-10 14:24:00 -0700

    added drawer so that when you click on names, a drawer will pop up showing you their record, fps, and points against

--- components/ui/matchupCard.tsx
+++ components/ui/matchupCard.tsx
@@ -1,15 +1,18 @@
 "use client"
 
-import React from 'react';
+import React, { useState } from 'react';
 import { FaTrophy } from "react-icons/fa6";
 import {
     Avatar,
     AvatarFallback,
     AvatarImage,
 } from "@/components/ui/avatar"
+import UserRecordDrawer from '@/app/userRecordDrawer';
 
 
 const MatchupCard = ({ team1, team2 }: { team1: any, team2: any }) => {
+    const [open, setOpen] = useState(false);
+    const [drawerTeam, setDrawerTeam] = useState();
 
     if (!team1 || !team2 || !team1.user || !team2.user) {
         return <div>Loading matchups...</div>;
@@ -21,40 +24,49 @@ const MatchupCard = ({ team1, team2 }: { team1: any, team2: any }) => {
         return null;
     };
 
+    const handleOpenDrawer = (team: any) => {
+        setOpen(true);
+        setDrawerTeam(team);
+        console.log(drawerTeam);
+    }
+
     return (
-        <div className="flex justify-evenly">
-            <div className="w-full flex items-center justify-between bg-white rounded-lg p-2 sm:p-4 my-2 text-black ring-1 ring-gray-200">
-                <div className="sm:w-[50%] w-[45%]">
-                    <div className="sm:flex items-center">
-                        <Avatar className='mr-4'>
-                            <AvatarImage src={`https://sleepercdn.com/avatars/thumbs/${team1.user.avatar}`} alt={`${team1.user.metadata.team_name} avatar`} />
-                            <AvatarFallback>{team1.user.display_name.charAt(0).toUpperCase() || ""}</AvatarFallback>
-                        </Avatar>
-                        <div className="flex flex-col mr-4">
-                            <span className="text-xs sm:text-sm text-gray-400 truncate">@{team1.user.display_name}</span>
-                            <span className="font-bold text-sm sm:text-lg truncate">{team1.user.metadata.team_name || team1.user.display_name}</span>
-                            <span className="inline-flex items-center text-sm text-gray-400 truncate">{team1.points} &nbsp; {getResult(team1.points, team2.points)}</span>
+        <>
+            <UserRecordDrawer open={open} setOpen={setOpen} drawerTeam={drawerTeam} />
+            <div className="flex justify-evenly">
+                <div className="w-full flex items-center justify-between bg-white rounded-lg p-2 sm:p-4 my-2 text-black ring-1 ring-gray-200">
+                    <div className="sm:w-[50%] w-[45%]">
+                        <div className="sm:flex items-center">
+                            <Avatar onClick={() => handleOpenDrawer(team1)} className='mr-4 cursor-pointer'>
+                                <AvatarImage src={`https://sleepercdn.com/avatars/thumbs/${team1.user.avatar}`} alt={`${team1.user.metadata.team_name} avatar`} />
+                                <AvatarFallback>{team1.user.display_name.charAt(0).toUpperCase() || ""}</AvatarFallback>
+                            </Avatar>
+                            <div className="flex flex-col mr-4">
+                                <span className="text-xs sm:text-sm text-gray-400 truncate">@{team1.user.display_name}</span>
+                                <span className="font-bold text-sm sm:text-lg truncate">{team1.user.metadata.team_name || team1.user.display_name}</span>
+                                <span className="inline-flex items-center text-sm text-gray-400 truncate">{team1.points} &nbsp; {getResult(team1.points, team2.points)}</span>
+                            </div>
+                            <span className="text-xl font-bold">{team1.score}</span>
                         </div>
-                        <span className="text-xl font-bold">{team1.score}</span>
                     </div>
-                </div>
-                <div className="text-sm sm:text-xl font-bold sm:mx-6">VS</div>
-                <div className="sm:w-[50%] w-[45%]">
-                    <div className="flex flex-col-reverse sm:flex-row justify-end">
-                        <span className="text-xl font-bold">{team2.score}</span>
-                        <div className="flex flex-col ml-4 mr-4">
-                            <span className="text-xs sm:text-sm text-gray-400 text-right truncate">@{team2.user.display_name}</span>
-                            <span className="font-bold text-sm sm:text-lg truncate text-right">{team2.user.metadata.team_name || team2.user.display_name}</span>
-                            <span className="inline-flex items-center justify-end text-sm text-gray-400 text-right truncate">{team2.points} &nbsp; {getResult(team2.points, team1.points)}</span>
+                    <div className="text-sm sm:text-xl font-bold sm:mx-6">VS</div>
+                    <div className="sm:w-[50%] w-[45%]">
+                        <div className="flex flex-col-reverse sm:flex-row justify-end">
+                            <span className="text-xl font-bold">{team2.score}</span>
+                            <div className="flex flex-col ml-4 mr-4">
+                                <span className="text-xs sm:text-sm text-gray-400 text-right truncate">@{team2.user.display_name}</span>
+                                <span className="font-bold text-sm sm:text-lg truncate text-right">{team2.user.metadata.team_name || team2.user.display_name}</span>
+                                <span className="inline-flex items-center justify-end text-sm text-gray-400 text-right truncate">{team2.points} &nbsp; {getResult(team2.points, team1.points)}</span>
+                            </div>
+                            <Avatar onClick={() => handleOpenDrawer(team2)} className='mr-4 sm:self-center self-end'>
+                                <AvatarImage src={`https://sleepercdn.com/avatars/thumbs/${team2.user.avatar}`} alt={`${team2.user.metadata.team_name} avatar`} />
+                                <AvatarFallback>{team2.user.display_name.charAt(0).toUpperCase() || ""}</AvatarFallback>
+                            </Avatar>
                         </div>
-                        <Avatar className='mr-4 sm:self-center self-end'>
-                            <AvatarImage src={`https://sleepercdn.com/avatars/thumbs/${team2.user.avatar}`} alt={`${team2.user.metadata.team_name} avatar`} />
-                            <AvatarFallback>{team2.user.display_name.charAt(0).toUpperCase() || ""}</AvatarFallback>
-                        </Avatar>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
