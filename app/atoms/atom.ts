cc75e547675b86370589a54800255e8a0aa8e9e3
bmaggiano <brandon.maggiano@gmail.com>
2024-07-10 11:50:12 -0700

    added league id atom

--- app/atoms/atom.ts
+++ app/atoms/atom.ts
@@ -1,5 +1,9 @@
 import { atom, useAtom } from 'jotai';
 
+// wetff = 974399495632891904
+// league of legends = 992142653368156160
+
 export const valueAtom = atom("Week 17");
 export const weekAtom = atom(17);
-export const leagueAtom = atom("974399495632891904")
+export const leagueAtom = atom("992142653368156160")
+export const leagueNameAtom = atom("Wet FF")
