96b43d49211ce73cb4a3ee572c2925d4391b7305
bmaggiano <brandon.maggiano@gmail.com>
2024-07-10 13:46:43 -0700

    added shadcn avatar with fall backs as well as naming fallbacks in case user doesn't have display name for whatever reason

--- components/ui/matchupCard.tsx
+++ components/ui/matchupCard.tsx
@@ -2,6 +2,11 @@
 
 import React from 'react';
 import { FaTrophy } from "react-icons/fa6";
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar"
 
 
 const MatchupCard = ({ team1, team2 }: { team1: any, team2: any }) => {
@@ -18,28 +23,34 @@ const MatchupCard = ({ team1, team2 }: { team1: any, team2: any }) => {
 
     return (
         <div className="flex justify-evenly">
-            <div className="w-full flex items-center justify-between bg-gray-800 rounded-lg p-2 sm:p-4 my-2 text-white">
+            <div className="w-full flex items-center justify-between bg-white rounded-lg p-2 sm:p-4 my-2 text-black ring-1 ring-gray-200">
                 <div className="sm:w-[50%] w-[45%]">
                     <div className="sm:flex items-center">
-                        <img src={`https://sleepercdn.com/avatars/thumbs/${team1.user.avatar}`} alt={`${team1.user.metadata.team_name} avatar`} className="w-12 h-12 rounded-full mr-4" />
+                        <Avatar className='mr-4'>
+                            <AvatarImage src={`https://sleepercdn.com/avatars/thumbs/${team1.user.avatar}`} alt={`${team1.user.metadata.team_name} avatar`} />
+                            <AvatarFallback>{team1.user.display_name.charAt(0).toUpperCase() || ""}</AvatarFallback>
+                        </Avatar>
                         <div className="flex flex-col mr-4">
                             <span className="text-xs sm:text-sm text-gray-400 truncate">@{team1.user.display_name}</span>
-                            <span className="font-bold text-sm sm:text-lg truncate">{team1.user.metadata.team_name}</span>
+                            <span className="font-bold text-sm sm:text-lg truncate">{team1.user.metadata.team_name || team1.user.display_name}</span>
                             <span className="inline-flex items-center text-sm text-gray-400 truncate">{team1.points} &nbsp; {getResult(team1.points, team2.points)}</span>
                         </div>
                         <span className="text-xl font-bold">{team1.score}</span>
                     </div>
                 </div>
-                <div className="text-xl font-bold sm:mx-6">VS</div>
+                <div className="text-sm sm:text-xl font-bold sm:mx-6">VS</div>
                 <div className="sm:w-[50%] w-[45%]">
                     <div className="flex flex-col-reverse sm:flex-row justify-end">
                         <span className="text-xl font-bold">{team2.score}</span>
                         <div className="flex flex-col ml-4 mr-4">
                             <span className="text-xs sm:text-sm text-gray-400 text-right truncate">@{team2.user.display_name}</span>
-                            <span className="font-bold text-sm sm:text-lg truncate text-right">{team2.user.metadata.team_name}</span>
+                            <span className="font-bold text-sm sm:text-lg truncate text-right">{team2.user.metadata.team_name || team2.user.display_name}</span>
                             <span className="inline-flex items-center justify-end text-sm text-gray-400 text-right truncate">{team2.points} &nbsp; {getResult(team2.points, team1.points)}</span>
                         </div>
-                        <img src={`https://sleepercdn.com/avatars/thumbs/${team2.user.avatar}`} alt={`${team2.user.metadata.team_name} avatar`} className="sm:self-center self-end w-12 h-12 rounded-full" />
+                        <Avatar className='mr-4 sm:self-center self-end'>
+                            <AvatarImage src={`https://sleepercdn.com/avatars/thumbs/${team2.user.avatar}`} alt={`${team2.user.metadata.team_name} avatar`} />
+                            <AvatarFallback>{team2.user.display_name.charAt(0).toUpperCase() || ""}</AvatarFallback>
+                        </Avatar>
                     </div>
                 </div>
             </div>
