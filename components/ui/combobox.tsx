0f37da2a8682adee4ec5c83579f6ceaa75d60cd6
bmaggiano <brandon.maggiano@gmail.com>
2024-07-09 21:38:16 -0700

    added shad cn combobox component

---
+++ components/ui/combobox.tsx
@@ -0,0 +1,75 @@
+"use client"
+"use client"
+
+import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+} from "@/components/ui/command";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import { CommandList } from "cmdk";
+import { useAtom } from "jotai";
+import { valueAtom, weekAtom } from "@/app/atoms/atom";
+
+export function Combobox({ data }: { data: { index: number, week: string }[] }) {
+    const [open, setOpen] = React.useState(false);
+    const [value, setValue] = useAtom(valueAtom);
+    const [weekIndex, setWeekIndex] = useAtom(weekAtom);
+
+    return (
+        <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+                <Button
+                    variant="outline"
+                    role="combobox"
+                    aria-expanded={open}
+                    className="w-[200px] justify-between"
+                >
+                    {value
+                        ? data.find((item) => item.week === value)?.week
+                        : "Select item..."}
+                    <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0">
+                <Command>
+                    <CommandList>
+                        <CommandInput placeholder="Search item..." />
+                        <CommandEmpty>No item found.</CommandEmpty>
+                        <CommandGroup>
+                            {data.map((item) => (
+                                <CommandItem
+                                    key={item.index}
+                                    value={item.week}
+                                    onSelect={() => {
+                                        setWeekIndex(item.index)
+                                        setValue(item.week === value ? "" : item.week);
+                                        setOpen(false);
+                                    }}
+                                >
+                                    <Check
+                                        className={cn(
+                                            "mr-2 h-4 w-4",
+                                            value === item.week ? "opacity-100" : "opacity-0"
+                                        )}
+                                    />
+                                    {item.week}
+                                </CommandItem>
+                            ))}
+                        </CommandGroup>
+                    </CommandList>
+                </Command>
+            </PopoverContent>
+        </Popover>
+    );
+}
