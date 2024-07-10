4f3758524eca0873ee6789a37ec4bec781fc69c4
bmaggiano <brandon.maggiano@gmail.com>
2024-07-10 09:59:54 -0700

    updated utils to bundle data together

--- app/utils.ts
+++ app/utils.ts
@@ -1,7 +1,57 @@
-
 "use server"
+
+const getUsersInfo = async (leagueId: string) => {
+    const response = await fetch(`https://api.sleeper.app/v1/league/${leagueId}/users`);
+    const userInfo = await response.json();
+    return userInfo;
+}
+
+const getRosterInfo = async (leagueId: string) => {
+    const response = await fetch(`https://api.sleeper.app/v1/league/${leagueId}/rosters`);
+    const rosterInfo = await response.json();
+    return rosterInfo;
+}
+
+const getMatchupInfo = async (leagueId: string, weekIndex: number) => {
+    const response = await fetch(`https://api.sleeper.app/v1/league/${leagueId}/matchups/${weekIndex}`);
+    const matchupInfo = await response.json();
+    return matchupInfo;
+}
+
+const combineUserAndRosterInfo = (userInfo: any, rosterInfo: any) => {
+    return rosterInfo.map((roster: any) => {
+        const user = userInfo.find((user: any) => user.user_id === roster.owner_id);
+        return {
+            ...roster,
+            user
+        }
+    });
+}
+
+const combineRosterAndMatchupInfo = (rosterInfo: any, matchupInfo: any) => {
+    return matchupInfo.map((matchup: any) => {
+        const roster = rosterInfo.find((roster: any) => roster.roster_id === matchup.roster_id);
+        return {
+            ...matchup,
+            ...roster
+        }
+    });
+}
+
+const getAllData = async (leagueId: string) => {
+    const userInfo = await getUsersInfo(leagueId);
+    const rosterInfo = await getRosterInfo(leagueId);
+    const combinedInfo = combineUserAndRosterInfo(userInfo, rosterInfo);
+    return combinedInfo;
+}
+
 export const getMatchups = async ({ weekIndex, leagueId }: { weekIndex: number, leagueId: string }) => {
-    const data = await fetch(`https://api.sleeper.app/v1/league/${leagueId}/matchups/${weekIndex}`)
-    const scoresData = await data.json()
-    return scoresData;
+    const userInfo = await getUsersInfo(leagueId);
+    const rosterInfo = await getRosterInfo(leagueId);
+    const matchupInfo = await getMatchupInfo(leagueId, weekIndex);
+
+    const combinedUserAndRosterInfo = combineUserAndRosterInfo(userInfo, rosterInfo);
+    const finalData = combineRosterAndMatchupInfo(combinedUserAndRosterInfo, matchupInfo);
+
+    return finalData;
 }
