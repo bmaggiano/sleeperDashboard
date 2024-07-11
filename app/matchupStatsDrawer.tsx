c5407a82a0566a6be9f7dd6dc455bc318635b43a
bmaggiano <brandon.maggiano@gmail.com>
2024-07-11 11:03:11 -0700

    modified matchup stats drawer to house stats data for that week

--- app/matchupStatsDrawer.tsx
+++ app/matchupStatsDrawer.tsx
@@ -2,12 +2,53 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
 import playerIds from "@/app/json/playerIds.json";
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { useEffect } from "react";
+import { useAtom } from "jotai";
+import { weekAtom } from "./atoms/atom";
+import MatchupCard from "@/components/ui/matchupCard";
+
+const getPlayerInfo = (playerId: string): Player => {
+    return (playerIds as { [key: string]: Player })[playerId] || { full_name: 'Unknown Player' };
+}
+
+interface PlayerMatchupCardsProps {
+    teamOne: Team;
+    teamTwo: Team;
+}
+
+function PlayerMatchupCards({ teamOne, teamTwo }: PlayerMatchupCardsProps) {
+    return (
+        <div className="flex flex-col space-y-1">
+            {teamOne.starters.map((starter, index) => (
+                <div key={index} className="flex justify-evenly items-center ring-1 ring-gray-200 rounded-md p-2">
+                    <div className="flex-1 flex flex-col justify-center items-start sm:items-center">
+                        <h1 className="text-xs sm:text-sm">{getPlayerInfo(starter).full_name}: {teamOne.starters_points[index]}</h1>
+                        <span className="text-xs sm:text-sm text-gray-500">{getPlayerInfo(starter).team}</span>
+                    </div>
+                    <div className="flex-initial flex justify-center items-center">
+                        <h1 className="text-xs sm:text-sm">{getPlayerInfo(starter).position}</h1>
+                    </div>
+                    <div className="flex-1 flex flex-col justify-center items-end sm:items-center">
+                        <h1 className="text-xs sm:text-sm">{getPlayerInfo(teamTwo.starters[index]).full_name}: {teamTwo.starters_points[index]}</h1>
+                        <span className="text-xs sm:text-sm text-gray-500">{getPlayerInfo(teamTwo.starters[index]).team}</span>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
 
 interface Player {
     full_name: string;
+    position: string;
+    team: string;
 }
 
 interface Team {
+    user: {
+        display_name: string;
+    };
     starters: string[];
     starters_points: number[];
 }
@@ -20,40 +61,28 @@ interface UserRecordDrawerProps {
 }
 
 const UserRecordDrawer: React.FC<UserRecordDrawerProps> = ({ open, setOpen, teamOne, teamTwo }) => {
+    const [week] = useAtom(weekAtom);
     if (!teamOne || !teamTwo) return null;
-    const getPlayerInfo = (playerId: string): Player => {
-        return (playerIds as { [key: string]: Player })[playerId] || { full_name: 'Unknown Player' };
-    }
-
     return (
         <Drawer open={open} onOpenChange={setOpen}>
-            <DrawerContent>
-                <div className="mx-auto w-full max-w-7xl">
-                    <DrawerHeader className="flex">
-                        <DrawerTitle>
-                            Matchup Stats
-                        </DrawerTitle>
-                    </DrawerHeader>
-                    <div className="flex justify-evenly">
-                        <div>
-                            <h1>Team One</h1>
-                            {teamOne.starters.map((starter, index) => (
-                                <h1 key={index}>{getPlayerInfo(starter).full_name}: {teamOne.starters_points[index]}</h1>
-                            ))}
-                        </div>
-                        <div>
-                            <h1>Team Two</h1>
-                            {teamTwo.starters.map((starter, index) => (
-                                <h1 key={index}>{getPlayerInfo(starter).full_name}: {teamTwo.starters_points[index]}</h1>
-                            ))}
-                        </div>
+            <DrawerContent className="max-h-screen flex flex-col">
+                <div className="flex-1 overflow-y-auto">
+                    <div className="mx-auto w-full max-w-3xl">
+                        <DrawerHeader className="flex">
+                            <DrawerTitle>
+                                Matchup Stats - Week {week}
+                            </DrawerTitle>
+                        </DrawerHeader>
+
+                        <MatchupCard team1={teamOne} team2={teamTwo} withVsLink={false} />
+                        <PlayerMatchupCards teamOne={teamOne} teamTwo={teamTwo} />
                     </div>
-                    <DrawerFooter>
-                        <DrawerClose asChild>
-                            <Button>Close</Button>
-                        </DrawerClose>
-                    </DrawerFooter>
                 </div>
+                <DrawerFooter className="max-w-3xl flex justify-center mx-auto w-full">
+                    <DrawerClose asChild>
+                        <Button className="w-full">Close</Button>
+                    </DrawerClose>
+                </DrawerFooter>
             </DrawerContent>
         </Drawer>
     )
