dfdb08c85e9811f312230622ba35f625f6f17e6e
bmaggiano <brandon.maggiano@gmail.com>
2024-07-11 15:14:08 -0700

    made league and league name empty

--- app/atoms/atom.ts
+++ app/atoms/atom.ts
@@ -5,5 +5,5 @@ import { atom, useAtom } from 'jotai';
 
 export const valueAtom = atom("Week 17");
 export const weekAtom = atom(17);
-export const leagueAtom = atom("974399495632891904")
-export const leagueNameAtom = atom("Wet FF")
+export const leagueAtom = atom("")
+export const leagueNameAtom = atom("")
