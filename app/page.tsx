6db0afc996135a6c96a874ab2bf8ebb99355f40a
bmaggiano <brandon.maggiano@gmail.com>
2024-07-11 15:13:33 -0700

    added recent search functionality to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,68 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import { Combobox } from "@/components/ui/combobox";
-import { useEffect } from "react";
-import Image from "next/image";
+import { useState } from "react";
 import { useAtom } from "jotai";
 import { valueAtom, leagueNameAtom, leagueAtom } from "./atoms/atom";
-import { getMatchups } from "./utils";
-import Scoreboard from "@/app/scoreboard";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { getLeagueName } from "./utils";
+import Scoreboard from "@/app/scoreboard";
+import RecentSearches from "@/components/recentSearches";
+import Header from "@/components/header";
+import { Separator } from "@/components/ui/separator";
 
 const weeks = Array.from({ length: 17 }, (_, i) => ({ index: i + 1, week: `Week ${i + 1}` }));
 
 export default function Home() {
   const [value, setValue] = useAtom(valueAtom);
+  const [localLeagueId, setLocalLeagueId] = useState<string>("");
   const [leagueId, setLeagueId] = useAtom(leagueAtom);
   const [leagueName, setLeagueName] = useAtom(leagueNameAtom);
+  const [error, setError] = useState<string | null>(null);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setLeagueName(leagueId);
+    setLeagueId(localLeagueId); // Set the leagueId atom
+    const response = await getLeagueName(localLeagueId);
+    if (response.error) {
+      setError(response.error);
+    } else {
+      setLeagueName(response);
+      const recentSearches = JSON.parse(localStorage.getItem("recentSearches") || "[]");
+      localStorage.setItem("recentSearches", JSON.stringify([...recentSearches, localLeagueId]));
+      setError(null);
+    }
   }
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLeagueId(e.target.value);
+    setLocalLeagueId(e.target.value);
   }
 
   return (
     <div className="p-2 sm:p-4">
-      <div className="w-full py-4">
-        <form className="flex justify-evenly items-center" onSubmit={handleSubmit}>
-          <Label className="w-[50%] text-base">Enter your Sleeper league ID:</Label>
-          <Input placeholder="974399495632891904" onChange={handleInputChange} value={leagueId} />
+      <Header />
+      <Separator />
+      <div className="w-full pt-4 pb-2">
+        <form onSubmit={handleSubmit}>
+          <Label className="text-base" htmlFor="leagueId">Enter your Sleeper league ID:</Label>
+          <div className="flex items-center">
+            <Input placeholder="ex. 992142653368156160" onChange={handleInputChange} value={localLeagueId} />
+            <Button className="ml-2" type="submit">Submit</Button>
+          </div>
         </form>
+        {error && <p className="text-red-500">{error}</p>}
       </div>
-      <div className="flex justify-between items-center">
-        <h1 className="font-medium">Matchups - {leagueName}</h1>
-        <Combobox data={weeks} />
-      </div>
-      <Scoreboard />
+      <RecentSearches />
+      {leagueId && leagueName && !error ?
+        <div className="py-2">
+          <div className="flex justify-between items-center">
+            <h1 className="font-medium">Matchups - {leagueName}</h1>
+            <Combobox data={weeks} />
+          </div>
+          <Scoreboard />
+        </div>
+        : null}
     </div>
   );
 }
