090cb4573217f9e78a68fb3859b625a8f07436d0
bmaggiano <brandon.maggiano@gmail.com>
2024-07-12 10:37:21 -0700

    rm error and added toast instead

--- app/leagueSearchForm.tsx
+++ app/leagueSearchForm.tsx
@@ -5,26 +5,32 @@ import { Input } from "@/components/ui/input";
 import { useAtom } from "jotai";
 import { valueAtom, leagueNameAtom, leagueAtom } from "./atoms/atom";
 import { getLeagueName } from "./utils";
+import { useToast } from "@/components/ui/use-toast";
+import { FiArrowRight } from "react-icons/fi";
+
 
 
 export default function LeagueSearchForm() {
+    const { toast } = useToast();
     const [value, setValue] = useAtom(valueAtom);
     const [localLeagueId, setLocalLeagueId] = useState<string>("");
     const [leagueName, setLeagueName] = useAtom(leagueNameAtom);
     const [leagueId, setLeagueId] = useAtom(leagueAtom);
-    const [error, setError] = useState<string | null>(null);
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         setLeagueId(localLeagueId); // Set the leagueId atom
         const response = await getLeagueName(localLeagueId);
         if (response.error) {
-            setError(response.error);
+            toast({
+                title: "Error",
+                description: response.error,
+            });
+            return;
         } else {
             setLeagueName(response);
             const recentSearches = JSON.parse(localStorage.getItem("recentSearches") || "[]");
             localStorage.setItem("recentSearches", JSON.stringify([...recentSearches, localLeagueId]));
-            setError(null);
         }
     }
 
@@ -33,14 +39,12 @@ export default function LeagueSearchForm() {
     }
     return (
 
-        <div className="w-full pt-4 pb-2">
+        <div className="sm:w-1/2 w-full mx-auto pt-4 pb-2">
             <form onSubmit={handleSubmit}>
-                <div className="flex flex-col justify-center items-center mx-auto">
-                    <Input placeholder="League ID ex. 992142653368156160" onChange={handleInputChange} value={localLeagueId} />
-                    <Button className="mt-2" type="submit">Submit</Button>
+                <div className="flex items-center">
+                    <Input placeholder="League ID ex. 992142653368156160" onChange={handleInputChange} value={localLeagueId} className="w-full" />
                 </div>
             </form>
-            {error && <p className="text-red-500">{error}</p>}
         </div>
     )
 }
