1ff2d5a125a0fbc10cf0c4ff386f4b71f05fecae
bmaggiano <brandon.maggiano@gmail.com>
2024-07-12 10:58:44 -0700

    early stages of popover setup, will eventually make this a gif

--- app/leagueSearchForm.tsx
+++ app/leagueSearchForm.tsx
@@ -7,6 +7,7 @@ import { valueAtom, leagueNameAtom, leagueAtom } from "./atoms/atom";
 import { getLeagueName } from "./utils";
 import { useToast } from "@/components/ui/use-toast";
 import { FiArrowRight } from "react-icons/fi";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 
 
 
@@ -39,12 +40,22 @@ export default function LeagueSearchForm() {
     }
     return (
 
-        <div className="sm:w-1/2 w-full mx-auto pt-4 pb-2">
+        <div className="sm:w-1/2 w-full mx-auto pt-4 pb-2 text-center">
             <form onSubmit={handleSubmit}>
                 <div className="flex items-center">
                     <Input placeholder="League ID ex. 992142653368156160" onChange={handleInputChange} value={localLeagueId} className="w-full" />
                 </div>
             </form>
+            <Popover>
+                <PopoverTrigger className="text-sm text-gray-500 mt-2 font-medium">Not sure how to find your league ID?</PopoverTrigger>
+                <PopoverContent>
+                    <ol className="list-decimal list-inside text-sm text-gray-500">
+                        <li>Go to the <a className="text-blue-500 underline" href="https://sleeper.app/leagues" target="_blank" rel="noreferrer">Leagues</a> page on the Sleeper website.</li>
+                        <li>Click on the league you want to search for.</li>
+                        <li>Copy the league ID from the URL.</li>
+                    </ol>
+                </PopoverContent>
+            </Popover>
         </div>
     )
 }
