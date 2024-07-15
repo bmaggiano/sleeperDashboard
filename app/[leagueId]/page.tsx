17e80864ef99acb954c4faf735065e214b7adcc2
bmaggiano <brandon.maggiano@gmail.com>
2024-07-15 16:20:37 -0700

    added league id page

---
+++ app/[leagueId]/page.tsx
@@ -0,0 +1,19 @@
+// page.tsx
+import ScoresComponent from "./scoresServer";
+import { Combobox } from "@/components/ui/combobox";
+
+export default function Page({ params }: { params: { leagueId: string } }) {
+    const { leagueId } = params;
+
+    const weeks = Array.from({ length: 17 }, (_, i) => ({ index: i + 1, week: `Week ${i + 1}` }));
+
+    return (
+        <main>
+            <div className="pt-4 pb-2 flex justify-between items-center">
+                <h1 className="font-medium">Matchups - {leagueId}</h1>
+                <Combobox leagueId={leagueId} data={weeks} />
+            </div>
+            <ScoresComponent leagueId={leagueId} week={1} />
+        </main>
+    );
+}
