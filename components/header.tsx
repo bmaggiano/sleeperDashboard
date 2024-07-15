cc1c46d3a07acd1fc6f07d6685a9c169be9f964e
bmaggiano <brandon.maggiano@gmail.com>
2024-07-15 16:29:46 -0700

    made logo clickable to return to home page

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
         <div className="flex flex-col sm:flex-row justify-between items-center py-2">
             <div className="flex items-center">
-                <img src="https://play-lh.googleusercontent.com/L5sDy5zFKKLLMndpR7wJfD3aum4w0FVL_rRK6W1t9T5-d4BYc-4A7LTXa2nGeP62TCo=w480-h960-rw" alt="Sleeper Fantasy Football Logo" className="h-8 w-8 mr-2 rounded-md" />
+                <Link href="/">
+                    <img src="https://play-lh.googleusercontent.com/L5sDy5zFKKLLMndpR7wJfD3aum4w0FVL_rRK6W1t9T5-d4BYc-4A7LTXa2nGeP62TCo=w480-h960-rw" alt="Sleeper Fantasy Football Logo" className="h-8 w-8 mr-2 rounded-md" />
+                </Link>
                 <h1 className="font-medium text-2xl">Fantasy Football Dashboard</h1>
             </div>
             <div className="flex items-center">
