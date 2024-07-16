105967c5eb5e6f607c95693766041d11efea240e
bmaggiano <brandon.maggiano@gmail.com>
2024-07-16 10:15:16 -0700

    added meta data to leauges

--- app/[leagueId]/[week]/page.tsx
+++ app/[leagueId]/[week]/page.tsx
@@ -3,6 +3,38 @@ import ScoresComponent from "../scoresServer";
 import { Combobox } from "@/components/ui/combobox";
 import { getLeagueName } from "../../utils";
 
+import { Metadata, ResolvingMetadata } from 'next';
+
+type Props = {
+    params: { leagueId: string, week: number }
+}
+
+export async function generateMetadata(
+    { params }: Props,
+    parent: ResolvingMetadata
+): Promise<Metadata> {
+    const id = params.leagueId;
+    const week = params.week;
+    const leagueName = await getLeagueName(id);
+
+    return {
+        title: `Matchups - ${leagueName} | Week ${week}`,
+        description: `Check out the matchups for ${leagueName} for week ${week}.`,
+        openGraph: {
+            title: `Matchups - ${leagueName} | Week ${week}`,
+            description: `Check out the matchups for ${leagueName} for week ${week}.`,
+            images: [
+                {
+                    url: `/path/to/custom/image/${id}.png`,
+                    width: 800,
+                    height: 600,
+                    alt: `Matchups for ${leagueName}`,
+                },
+            ],
+        },
+    };
+}
+
 const weeks = Array.from({ length: 17 }, (_, i) => ({ index: i + 1, week: `Week ${i + 1}` }));
 
 export default function WeekMatchup({ params }: { params: { week: number, leagueId: string } }) {
