59af451327a9451533deb920e6d2a9f70aba0837
bmaggiano <brandon.maggiano@gmail.com>
2024-07-16 10:44:38 -0700

    updated image path

--- app/[leagueId]/[week]/page.tsx
+++ app/[leagueId]/[week]/page.tsx
@@ -25,7 +25,7 @@ export async function generateMetadata(
             description: `Check out the matchups for ${leagueName} for week ${week}.`,
             images: [
                 {
-                    url: `/image.png`,
+                    url: `image.png`,
                     width: 800,
                     height: 600,
                     alt: `Matchups for ${leagueName}`,
