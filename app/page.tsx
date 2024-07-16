7598bfb66d253e31047d8d001b69b5765535a168
bmaggiano <brandon.maggiano@gmail.com>
2024-07-16 09:22:24 -0700

    main page rm files

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
 "use client";
 import { useAtom } from "jotai";
 import { leagueNameAtom, leagueAtom } from "./atoms/atom";
-import Scoreboard from "@/app/scoreboard";
 import RecentSearches from "@/components/recentSearches";
-import Header from "@/components/header";
-import { Separator } from "@/components/ui/separator";
 import { MarqueeDemo } from "./exampleLeagues";
 import LeagueSearchForm from "./leagueSearchForm";
 
 export default function Home() {
-  const [leagueName] = useAtom(leagueNameAtom);
-  const [leagueId] = useAtom(leagueAtom);
 
   return (
     <div className="p-2 sm:p-4 max-w-3xl mx-auto">
