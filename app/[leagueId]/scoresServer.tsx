2b427a12fe3907b2f792e4067d3dad38db315b41
bmaggiano <brandon.maggiano@gmail.com>
2024-07-18 16:51:34 -0700

    updated scores to scores client

--- app/[leagueId]/scoresServer.tsx
+++ app/[leagueId]/scoresServer.tsx
@@ -1,9 +1,9 @@
 import { getMatchups } from "../utils";
-import { Score } from "./scoresClient";
+import { ScoreClient } from "./scoresClient";
 
 async function ScoresComponent({ leagueId, week }: { leagueId: string, week: number }) {
     const scores = await getMatchups({ weekIndex: week, leagueId });
-    return <Score scoresData={scores} />;
+    return <ScoreClient scoresData={scores} />;
 }
 
 export default ScoresComponent;
