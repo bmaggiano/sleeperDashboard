d03a1fbec9b501e96549ea372895da04f5194e33
bmaggiano <brandon.maggiano@gmail.com>
2024-07-31 14:55:04 -0700

    updated layout for testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
   description: "Your one-stop dashboard for all your Fantasy data.",
   openGraph: {
     images: [{
-      url: `https://sleeper-dashboard.vercel.app/api/og`,
+      url: `https://sleeper-dashboard.vercel.app/api/cards`,
       width: 1200,
       height: 630,
       alt: 'Fantasy Dashboard',
