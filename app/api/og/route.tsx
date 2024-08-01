9ed9be0a095182ee05206a9ce6843586f81e2c53
bmaggiano <brandon.maggiano@gmail.com>
2024-08-01 16:01:26 -0700

    runtime fix

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -6,9 +6,7 @@ import path from "path"
 import { getAppUrl } from "@/lib/utils"
 import { ogImageSchema } from "@/lib/validations/og"
 
-export const config = {
-  runtime: "edge",
-}
+export const config = { runtime: "edge" }
 
 // Function to read font files from disk
 // Function to read font files from disk
