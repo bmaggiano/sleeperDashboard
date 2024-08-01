77a9c91fbd509291b8a85314cd5fc640c55fe980
bmaggiano <brandon.maggiano@gmail.com>
2024-08-01 10:41:38 -0700

    ternary logic for trophies as well as fallback values

--- app/[leagueId]/[week]/[matchup]/page.tsx
+++ app/[leagueId]/[week]/[matchup]/page.tsx
@@ -34,6 +34,8 @@ export async function generateMetadata(
         teamTwoDisplayName: data[1].user.display_name,
         teamOnePoints: data[0].points,
         teamTwoPoints: data[1].points,
+        teamOneWin: (data[0].points > data[1].points) ? true : false,
+        teamTwoWin: (data[1].points > data[0].points) ? true : false,
         teamOneAvatar: data[0].user.metadata.avatar || "https://via.placeholder.com/64",
         teamTwoAvatar: data[1].user.metadata.avatar || "https://via.placeholder.com/64",
     }
--- app/api/cards/route.tsx
+++ app/api/cards/route.tsx
@@ -28,7 +28,10 @@ export async function GET(req: NextRequest) {
                             <div tw="flex flex-col mr-4">
                                 <span tw="text-sm text-2xl text-[#9CA3AF] overflow-hidden overflow-ellipsis">{paramsObj.teamOneDisplayName}</span>
                                 <span tw="font-bold text-3xl overflow-hidden overflow-ellipsis">{paramsObj.teamOneName}</span>
-                                <span tw="flex items-center text-2xl text-[#6B7280] overflow-hidden overflow-ellipsis">{paramsObj.teamOnePoints}</span>
+                                <span tw="flex items-center text-2xl text-[#6B7280] overflow-hidden overflow-ellipsis">
+                                    {paramsObj.teamOnePoints}
+                                    {paramsObj.teamOneWin ? <span tw="mr-1 text-[#FFD700] text-2xl">🏆</span> : null}
+                                </span>
                             </div>
                         </div>
 
@@ -44,7 +47,7 @@ export async function GET(req: NextRequest) {
                                 <span tw="font-bold text-3xl overflow-hidden overflow-ellipsis text-right">{paramsObj.teamTwoName}</span>
                                 <span tw="flex items-center text-2xl text-[#6B7280] overflow-hidden overflow-ellipsis text-right">
                                     {paramsObj.teamTwoPoints}
-                                    <span tw="ml-1 text-[#FFD700] text-2xl">🏆</span>
+                                    {paramsObj.teamTwoWin ? <span tw="ml-1 text-[#FFD700] text-2xl">🏆</span> : null}
                                 </span>
                             </div>
                             <img tw="h-20 w-20 rounded-full ml-4" src={`${paramsObj.teamTwoAvatar}`} />
