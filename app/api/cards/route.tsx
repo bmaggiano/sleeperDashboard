6d777acaf13c33bb3cdf448afd0f70f1317e6a3d
bmaggiano <brandon.maggiano@gmail.com>
2024-08-01 17:35:38 -0700

    re added images to matchup careds and gave route.tsx a final touch up of og

--- app/api/cards/route.tsx
+++ app/api/cards/route.tsx
@@ -31,7 +31,7 @@ export async function GET(req: NextRequest) {
                     <div tw="flex items-center mx-auto">
                         {/* Left Side Team Info */}
                         <div tw="flex items-center w-2/5 pl-8">
-                            {/* <img tw="h-20 w-20 mr-4 rounded-full" src={`${paramsObj.teamOneAvatar}`} /> */}
+                            <img tw="h-20 w-20 mr-4 rounded-full" src={`${paramsObj.teamOneAvatar}`} />
                             <div tw="flex flex-col mr-4">
                                 <span
                                     tw="text-sm text-3xl text-[#9CA3AF] overflow-hidden overflow-ellipsis"
@@ -107,7 +107,7 @@ export async function GET(req: NextRequest) {
                                     {paramsObj.teamOneWin === "false" ? <span tw="ml-1 text-[#FFD700] text-3xl">🏆</span> : null}
                                 </span>
                             </div>
-                            {/* <img tw="h-20 w-20 rounded-full ml-4" src={`${paramsObj.teamTwoAvatar}`} /> */}
+                            <img tw="h-20 w-20 rounded-full ml-4" src={`${paramsObj.teamTwoAvatar}`} />
                         </div>
                     </div>
                 </div>
--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -31,34 +31,39 @@ export async function GET(req: NextRequest) {
             >
               {/* {values.type} */}
             </div>
-            <div
-              tw="flex leading-[1.1] text-white text-[80px] font-bold tracking-tighter"
-              style={{
-                fontFamily: "Inter",
-                fontWeight: "bolder",
-                marginLeft: "-3px",
-                fontSize,
-              }}
-            >
-              Stuart Ai
+            <div tw="flex">
+              <p
+                tw="leading-[1.1] text-white text-[80px] font-bold tracking-tighter mb-6"
+                style={{
+                  fontFamily: "Inter",
+                  fontWeight: "bolder",
+                  marginLeft: "-3px",
+                  fontSize,
+                }}
+              >
+                Stuart
+                <span tw="ml-6 bg-white text-black pl-4 pr-6 rounded-md">
+                  AI
+                </span>
+              </p>
             </div>
-            <div tw="flex leading-[1.1] text-gray-400 text-[50px] font-bold">The power of AI in Fantasy Football</div>
+            <div tw="flex leading-[1.1] text-gray-400 text-[50px] font-bold">Your fantasy football knowledge base</div>
           </div>
-          <div tw="flex flex-col space-y-8 items-start w-full justify-between">
-            <div tw="flex items-center space-x-2">
-              {/* <img
-                src={`${getAppUrl()}/headshot.jpg`}
-                tw="h-12 w-12 rounded-full mr-2"
-                alt="sidd"
-              /> */}
-              <div tw="font-bold">Sidd</div>
-              <div tw="font-normal">harth Sharma</div>
-            </div>
-            <div
-              tw="flex text-xl mt-4"
-              style={{ fontFamily: "Inter", fontWeight: "normal" }}
-            >
-              thesiddd.com
+          <div tw="flex flex-col items-start w-full">
+            <div tw="flex flex-row items-center">
+              <img
+                src="https://sleeper-dashboard.vercel.app/logo.png"
+                tw="h-12 w-12 rounded-full mr-4"
+                alt="football"
+              />
+              <div tw="flex flex-col items-start font-bold text-gray-400">
+                <p tw="mb-0">
+                  Connect with Sleeper
+                </p>
+                <p tw="mt-0">
+                  v1.0.0
+                </p>
+              </div>
             </div>
           </div>
         </div>
