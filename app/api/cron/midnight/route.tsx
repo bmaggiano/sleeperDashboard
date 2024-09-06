b27a2f10593e6052f983c88283b6ec1171210cbb
bmaggiano <brandon.maggiano@gmail.com>
2024-09-06 11:00:24 -0700

    updated cron schedule logic

---
+++ app/api/cron/midnight/route.tsx
@@ -0,0 +1,21 @@
+// app/api/cron/midnight/route.tsx
+
+import { NextResponse } from 'next/server'
+import prisma from '@/lib/db'
+
+export async function GET() {
+  try {
+    console.log('Resetting daily limits...')
+
+    // Reset daily limits for all users
+    await prisma.user.updateMany({
+      data: { dailyLimit: 10 } as any,
+    })
+
+    console.log('Daily limits have been reset.')
+    return NextResponse.json({ message: 'Daily limits reset successfully.' })
+  } catch (error) {
+    console.error('Error resetting daily limits:', error)
+    return NextResponse.error()
+  }
+}
