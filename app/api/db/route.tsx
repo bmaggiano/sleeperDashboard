9cb807bbe630949971bb88ec707e65f8f2fc2f89
bmaggiano <brandon.maggiano@gmail.com>
2024-09-06 13:29:29 -0700

    v0 second suggestions

--- app/api/db/route.tsx
+++ app/api/db/route.tsx
@@ -1,6 +1,6 @@
 import db from '@/lib/db'
 import { getServerSession } from 'next-auth/next'
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { authOptions } from '@/app/api/auth/[...nextauth]/options'
 import { NextRequest, NextResponse } from 'next/server'
 import redisClient from '@/lib/redis/redisClient'
 import { getPlayerDetails } from '@/lib/sleeper/helpers'
--- app/api/stats/route.tsx
+++ app/api/stats/route.tsx
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import db from '@/lib/db'
 import { getServerSession } from 'next-auth/next'
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { authOptions } from '@/app/api/auth/[...nextauth]/options'
 import { NextRequest } from 'next/server'
 import redisClient from '@/lib/redis/redisClient'
 import { getPlayerDetails } from '@/lib/sleeper/helpers'
