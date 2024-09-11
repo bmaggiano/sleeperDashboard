a0050bd8e2d7e8ef04236ef7ee3d917df35ee543
bmaggiano <brandon.maggiano@gmail.com>
2024-09-11 11:39:39 -0700

    updated playerCompareOg to be skeleton if no data in the query params

--- app/api/playerCompareOg/route.tsx
+++ app/api/playerCompareOg/route.tsx
@@ -6,21 +6,106 @@ export const runtime = 'edge'
 const font1 = fetch(
   new URL('/public/assets/fonts/Inter-Bold.ttf', import.meta.url)
 ).then((res) => res.arrayBuffer())
-// Use a named export for the GET method
+
+const fontSize = '80px'
+
 export async function GET(req: NextRequest) {
   try {
     const fontData1 = await font1
 
-    // Create a URL object from the request URL
     const { searchParams } = new URL(req.url)
     let paramsObj: Record<string, string | string[]> = {}
     searchParams.forEach((value, key) => {
       paramsObj[key] = value
     })
 
-    const fontSize = '80px'
+    // Check if required params are missing and render skeleton if so
+    if (!paramsObj.p1EId || !paramsObj.p2EId) {
+      return new ImageResponse(
+        (
+          <div
+            tw="flex relative flex-col p-12 w-full h-full items-center"
+            style={{
+              color: 'fff',
+              background: 'white',
+            }}
+          >
+            <div tw="flex flex-col py-10">
+              <div tw="flex items-center">
+                <p
+                  tw="leading-[1.1] text-black text-[80px] font-bold tracking-tighter mb-6"
+                  style={{
+                    fontFamily: 'Inter',
+                    fontWeight: 'bolder',
+                    marginLeft: '-3px',
+                    fontSize,
+                  }}
+                >
+                  Stuart
+                  <span tw="ml-6 bg-black text-white pl-4 pr-6 rounded-md">
+                    AI
+                  </span>
+                </p>
+                <p tw="ml-4 flex leading-[1.1] text-gray-400 text-[80px] font-bold tracking-tighter">
+                  /Player Compare
+                </p>
+              </div>
+              <div tw="flex justify-between items-center py-10">
+                <div tw="flex items-center">
+                  <div
+                    tw="bg-gray-300 rounded-full"
+                    style={{ height: '160px', width: '160px' }}
+                  />
+                  <div tw="flex text-black flex-col justify-start ml-6">
+                    <p
+                      tw="bg-gray-300 rounded-md"
+                      style={{ width: '300px', height: '40px' }}
+                    />
+                    <p
+                      tw="bg-gray-200 rounded-md mt-2"
+                      style={{ width: '200px', height: '30px' }}
+                    />
+                  </div>
+                </div>
 
-    // Return an Open Graph image response
+                <div tw="flex items-center">
+                  <div
+                    tw="bg-gray-300 rounded-full"
+                    style={{ height: '160px', width: '160px' }}
+                  />
+                  <div tw="flex text-black flex-col justify-start ml-6">
+                    <p
+                      tw="bg-gray-300 rounded-md"
+                      style={{ width: '300px', height: '40px' }}
+                    />
+                    <p
+                      tw="bg-gray-200 rounded-md mt-2"
+                      style={{ width: '200px', height: '30px' }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <p tw="mb-0 ml-2 text-gray-500">Powered by OpenAI</p>
+          </div>
+        ),
+        {
+          width: 1200,
+          height: 630,
+          fonts: [
+            {
+              name: 'Inter-Bold',
+              data: fontData1,
+              style: 'normal',
+            },
+          ],
+        }
+      )
+    }
+
+    // Existing code to render image with actual player data
+
     return new ImageResponse(
       (
         <div
@@ -31,12 +116,6 @@ export async function GET(req: NextRequest) {
           }}
         >
           <div tw="flex flex-col py-10">
-            <div
-              tw="flex text-xl uppercase font-bold tracking-tight"
-              style={{ fontFamily: 'Inter', fontWeight: 'normal' }}
-            >
-              {/* {values.type} */}
-            </div>
             <div tw="flex items-center">
               <p
                 tw="leading-[1.1] text-black text-[80px] font-bold tracking-tighter mb-6"
@@ -72,7 +151,7 @@ export async function GET(req: NextRequest) {
                       overflow: 'hidden',
                       textOverflow: 'ellipsis',
                       fontSize: '40px',
-                      maxWidth: '300px', // Adjust width to control truncation
+                      maxWidth: '300px',
                     }}
                   >
                     {paramsObj.p1Name}
@@ -83,7 +162,7 @@ export async function GET(req: NextRequest) {
                 </div>
               </div>
 
-              <div tw="flex p-6 rounded-md border max-w-1/2 border-[#E5E7EB]">
+              <div tw="flex p-6 rounded-md border border-[#E5E7EB]">
                 <img
                   tw="rounded-full"
                   height={120}
@@ -98,7 +177,7 @@ export async function GET(req: NextRequest) {
                       overflow: 'hidden',
                       textOverflow: 'ellipsis',
                       fontSize: '40px',
-                      maxWidth: '300px', // Adjust width to control truncation
+                      maxWidth: '300px',
                     }}
                   >
                     {paramsObj.p2Name}
